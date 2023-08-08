010213f95f66e34c2174a8c324ef9e5fd9c2869a
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-08 14:19:28 +0200

    something is not correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Navigation from "./routes/navigation/navigation.route";
 import Step1 from "./routes/step1/step1.route";
 import Step2 from "./routes/step2/step2.route";
 import Step3 from "./routes/step3/step3.route";
@@ -10,10 +11,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/step1" element={Step1} />
-        <Route path="/step2" element={Step2} />
-        <Route path="/step3" element={Step3} />
-        <Route path="/step4" element={Step4} />
+        <Route path="/" element={Navigation}>
+          <Route index element={Step1} />
+          <Route path="step2" element={Step2} />
+        </Route>
       </Routes>
     </>
   );
