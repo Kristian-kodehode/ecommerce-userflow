4a23dcbcd98488260c4ce2872e455c940c55c5a4
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-08 14:28:14 +0200

    nothing works

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
-import Navigation from "./routes/navigation/navigation.route";
-import Step1 from "./routes/step1/step1.route";
-import Step2 from "./routes/step2/step2.route";
-import Step3 from "./routes/step3/step3.route";
-import Step4 from "./routes/step4/step4.route";
+import Step1 from "./routes/step1/step1.route.jsx";
+import Step2 from "./routes/step2/step2.route.jsx";
+import Step3 from "./routes/step3/step3.route.jsx";
+import Step4 from "./routes/step4/step4.route.jsx";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={Navigation}>
-          <Route index element={Step1} />
-          <Route path="step2" element={Step2} />
-        </Route>
-      </Routes>
-    </>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
 }
 
