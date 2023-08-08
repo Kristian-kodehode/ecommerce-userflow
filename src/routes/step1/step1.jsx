362d5fe28c065c965eb92bd8d64cfc5f970aca47
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-08 14:54:58 +0200

    added a reminder comment on step1 ui

--- src/routes/step1/step1.jsx
+++ src/routes/step1/step1.jsx
@@ -5,6 +5,7 @@ const Step1 = () => {
   return (
     <div>
       <h1>Step 1: Select Item</h1>
+      <h3>This app is ready for styling now 😃</h3>
       {/* Add your content for selecting an item here */}
       <Link to="/step2">Next Step</Link>
     </div>
