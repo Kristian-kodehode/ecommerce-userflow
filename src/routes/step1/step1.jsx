c0ae5f146413bbc208ff2909b4b0e4a921b992f7
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-09 17:20:28 +0200

    added fake butikk henting. also added css module

--- src/routes/step1/step1.jsx
+++ src/routes/step1/step1.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Step1.module.css";
 
 const Step1 = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setProducts(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div>
+    <div className={styles.step1Container}>
       <h1>Step 1: Select Item</h1>
       <h3>This app is ready for styling now 😃</h3>
+      <ul className={styles.productList}>
+        {products.map((product) => {
+          return (
+            <div key={product.id} className={styles.productCard}>
+              <img src={product.image} alt="" />
+              <h3>{product.title}</h3>
+              <h3>$ {product.price}</h3>
+            </div>
+          );
+        })}
+      </ul>
       {/* Add your content for selecting an item here */}
       <Link to="/step2">Next Step</Link>
     </div>
