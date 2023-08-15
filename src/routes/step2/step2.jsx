e849ca6cdc4ed8aefda30d0737e64b12ccaae9f6
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-15 19:17:16 +0200

    The cart seem to work now. need totalprice and css

--- src/routes/step2/step2.jsx
+++ src/routes/step2/step2.jsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 import { useCart } from "../../CartContext";
 
 const Step2 = () => {
-  const { selectedItems } = useCart();
-  console.log("selectedItems");
+  const { selectedItems, setSelectedItems } = useCart([]);
+
+  const handleRemove = (productId) => {
+    setSelectedItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== productId);
+    });
+  };
+
+  console.log(selectedItems);
   return (
     <div>
       <h1>Step 2: Item Added to Basket</h1>
@@ -13,6 +20,7 @@ const Step2 = () => {
         {selectedItems.map((item, index) => (
           <li key={index}>
             {item.title} - ${item.price}
+            <button onClick={() => handleRemove(item.id)}>remove item</button>
           </li>
         ))}
       </ul>
