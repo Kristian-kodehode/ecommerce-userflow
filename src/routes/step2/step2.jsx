6cb8e172f948b5c346c57c591f6acb170ccd24f7
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-15 19:24:13 +0200

    step 2 cart has now total amount. needs css

--- src/routes/step2/step2.jsx
+++ src/routes/step2/step2.jsx
@@ -11,11 +11,16 @@ const Step2 = () => {
     });
   };
 
+  const totalAmount = selectedItems.reduce(
+    (total, item) => total + item.price,
+    0
+  );
+
   console.log(selectedItems);
   return (
     <div>
-      <h1>Step 2: Item Added to Basket</h1>
-      {/* Add your content for displaying the basket here */}
+      <h1>Step 2: Shoppingcart</h1>
+      {/* List of cart items below in ul */}
       <ul>
         {selectedItems.map((item, index) => (
           <li key={index}>
@@ -24,8 +29,9 @@ const Step2 = () => {
           </li>
         ))}
       </ul>
+      <h2>Total Amount: $ {totalAmount.toFixed(2)}</h2>
 
-      <Link to="/step3">Next Step</Link>
+      <Link to="/step3">Pay in step3</Link>
     </div>
   );
 };
