06aa365d2db621361b0110b1e2797de99188fd61
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-17 09:33:51 +0200

    added some brief content to about page

--- src/routes/aboutproject/aboutproject.jsx
+++ src/routes/aboutproject/aboutproject.jsx
@@ -1,12 +1,23 @@
+import styles from "./aboutproject.module.css";
+
 const AboutProject = () => {
   return (
     <>
-      <h1>Title</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-        maxime.
-      </p>
+      <div className={styles.textwrapper}>
+        <h1 className={styles.header}>About this fictional shop</h1>
+        <p>
+          This is a mini eCommerce project where a fakestore is used to have
+          products. It is mainly a 4 step process.
+        </p>
+        <ol className={styles.orderedlist}>
+          <li>Select item or items.</li>
+          <li>
+            Overview of shoppingcart. Also possible to edit item amount in cart.
+          </li>
+          <li>Checkout section where you enter your fake creditcard info.</li>
+          <li>Shop gives you a visual verification that its a success.</li>
+        </ol>
+      </div>
     </>
   );
 };
