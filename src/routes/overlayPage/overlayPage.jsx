aa695f6674ff42533f6d83375e9a20cd371496f8
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-25 15:01:53 +0200

    working on overlayPage

--- src/routes/overlayPage/overlayPage.jsx
+++ src/routes/overlayPage/overlayPage.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./overlayPage.module.css";
 // import step2 from "../step2/step2";
 
 const OverlayPage = () => {
   return (
-    <div>
+    <div className={styles.pagecontainer}>
       <div>
-        <h3>Item Added to Cart</h3>
-        <p> has been added to your cart.</p>
+        <h3>Item Successfully Added to Cart</h3>
       </div>
-      <Link to="/">Continue shopping</Link>
-      <Link to="/step2">Go to Cart</Link>
+
+      <div className={styles.ctacontainer}>
+        <Link to="/" className={styles.linkbuttons}>
+          Continue shopping
+        </Link>
+        <span>or</span>
+        <Link to="/step2" className={styles.linkbuttons}>
+          Go to Cart
+        </Link>
+      </div>
     </div>
   );
 };
