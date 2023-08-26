650f95a65d3e931bff46ca95dc036cfcae632fba
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-26 17:10:44 +0200

    counter quantity in cart now works great

--- src/routes/step2/step2.jsx
+++ src/routes/step2/step2.jsx
@@ -2,9 +2,11 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../../CartContext";
 import styles from "./step2.module.css";
+import { useState } from "react";
 
 const Step2 = () => {
   const { selectedCartItems, setSelectedCartItems } = useCart([]);
+  const [counter, setCounter] = useState({});
 
   const handleRemoveItem = (productId) => {
     setSelectedCartItems((prevItems) => {
@@ -12,13 +14,26 @@ const Step2 = () => {
     });
   };
 
-  const totalAmount = selectedCartItems.reduce(
-    (total, item) => total + item.price,
-    0
-  );
+  // const totalAmount = selectedCartItems.reduce(
+  //   (total, item) => total + item.price,
+  //   0
+  // );
+
+  const totalAmount = selectedCartItems.reduce((total, item) => {
+    // Calculate the adjusted price based on the multiplier (default to 1 if no counter exists)
+    const multiplier = counter[item.id] || 1;
+    return total + item.price * multiplier;
+  }, 0);
 
   const handleClearCart = () => {
     setSelectedCartItems([]);
+  };
+
+  const handleCounterChange = (productId, newCounter) => {
+    setCounter((prevCounter) => ({
+      ...prevCounter,
+      [productId]: newCounter,
+    }));
   };
 
   console.log(selectedCartItems);
@@ -32,18 +47,34 @@ const Step2 = () => {
             <img className={styles.productimage} src={item.image} alt="" />
             <div className={styles.productinfo}>
               <h4>{item.title}</h4>
+
               <span className={styles.quantityandpricecontainer}>
                 <span className={styles.quantitycontainer}>
-                  <button className={styles.buttonquantity}>-</button>
-                  <input
-                    className={styles.inputcontainer}
-                    type="text"
-                    value="1"
-                  />
-                  <button className={styles.buttonquantity}>+</button>
+                  <button
+                    className={styles.buttonquantity}
+                    onClick={() =>
+                      handleCounterChange(item.id, (counter[item.id] || 1) - 1)
+                    }
+                  >
+                    -
+                  </button>
+                  <span className={styles.quantitydisplay}>
+                    {counter[item.id] || 1}
+                  </span>{" "}
+                  <button
+                    className={styles.buttonquantity}
+                    onClick={() =>
+                      handleCounterChange(item.id, (counter[item.id] || 1) + 1)
+                    }
+                  >
+                    +
+                  </button>
                 </span>
+
                 <span>
-                  <span>$ {item.price}</span>
+                  <span>
+                    $ {(item.price * (counter[item.id] || 1)).toFixed(2)}
+                  </span>
                   <button
                     className={styles.buttonremove}
                     onClick={() => handleRemoveItem(item.id)}
