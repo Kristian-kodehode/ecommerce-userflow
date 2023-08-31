60e70f72d3829bec857da98fd94ea09b8a478f78
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-31 11:33:25 +0200

    updated the default value of datepicker

--- src/components/checkoutForm/checkoutForm.jsx
+++ src/components/checkoutForm/checkoutForm.jsx
@@ -5,7 +5,7 @@ const CheckoutForm = (props) => {
   const todayDate = new Date().toISOString().split("T")[0];
   const [cardType, setCardType] = useState("");
   const [cardNumber, setCardNumber] = useState("");
-  const [expiration, setExpiration] = useState("Hello");
+  const [expiration, setExpiration] = useState(todayDate);
   const [cvc, setCvc] = useState("");
 
   const handleSubmit = (event) => {
